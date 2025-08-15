049397ba8ae58957c152d46d71839621daf1f882
Bal Krishna Ojha <balkrishna0488@gmail.com>
2025-08-15 11:55:49 +0530

    deploying setup

--- Frontend/bharat-college-of-technology/src/CommonComponents/ApiEndpoints.js
+++ Frontend/bharat-college-of-technology/src/CommonComponents/ApiEndpoints.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const baseUrl = 'http://localhost:4000/api'
-const baseUrl=process.env.REACT_APP_API_BASE_URL
+const baseUrl=`${process.env.REACT_APP_API_BASE_URL}/api`
 
 const ApiEndpoints = {
   // Signup Endpoints
